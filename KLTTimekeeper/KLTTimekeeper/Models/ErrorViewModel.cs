9a29e9e7205773db06840db1bf05d5b475caddfb
Tyler Barnes <chalcanthite20@gmail.com>
2018-03-19 21:10:31 -0600

    Initial

---
+++ KLTTimekeeper/KLTTimekeeper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KLTTimekeeper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
