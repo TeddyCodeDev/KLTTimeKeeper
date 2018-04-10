22f2a548b21312f49d727e2183d516b6d8c844e4
dprprunning <liyaoyang@mail.weber.edu>
2018-04-09 20:49:20 -0600

    Migration

--- KLTTimekeeper/KLTTimekeeper/Data/Migrations/20180410024733_UpdateAll.Designer.cs
+++ KLTTimekeeper/KLTTimekeeper/Data/Migrations/20180410024733_UpdateAll.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace KLTTimekeeper.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180410024733_UpdateAll")]
+    partial class UpdateAll
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ KLTTimekeeper/KLTTimekeeper/Data/Migrations/20180410024733_UpdateAll.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace KLTTimekeeper.Data.Migrations
+{
+    public partial class UpdateAll : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CourseID",
+                table: "Project",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "dateCreated",
+                table: "Project",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "dateDue",
+                table: "Project",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "projectName",
+                table: "Project",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    CourseID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ApplicationUserId = table.Column<string>(nullable: true),
+                    CourseName = table.Column<string>(nullable: true),
+                    InstructorID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.CourseID);
+                    table.ForeignKey(
+                        name: "FK_Course_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_CourseID",
+                table: "Project",
+                column: "CourseID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Course_ApplicationUserId",
+                table: "Course",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Course_CourseID",
+                table: "Project",
+                column: "CourseID",
+                principalTable: "Course",
+                principalColumn: "CourseID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Course_CourseID",
+                table: "Project");
+
+            migrationBuilder.DropTable(
+                name: "Course");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Project_CourseID",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "CourseID",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "dateCreated",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "dateDue",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "projectName",
+                table: "Project");
+        }
+    }
+}
