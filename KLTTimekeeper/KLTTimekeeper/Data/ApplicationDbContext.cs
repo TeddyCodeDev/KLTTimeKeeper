107c4a4fc3013c7a39d2adc0ff294ca19c8919af
Kaitlyn Kakazu <KaitlynKakazu@gmail.com>
2018-04-25 19:58:37 -0600

    Making sure this commits

--- KLTTimekeeper/KLTTimekeeper/Data/ApplicationDbContext.cs
+++ KLTTimekeeper/KLTTimekeeper/Data/ApplicationDbContext.cs
@@ -31,5 +31,7 @@ namespace KLTTimekeeper.Data
         public DbSet<KLTTimekeeper.Models.TimeCard> TimeCard { get; set; }
 
         public DbSet<KLTTimekeeper.Models.Course> Course { get; set; }
+
+        public DbSet<KLTTimekeeper.Models.Member> Member { get; set; }
     }
 }
